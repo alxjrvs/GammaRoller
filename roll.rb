cec7e6f837b1dd0150aa793a1e7ecb302be63f61
Alex Jarvis <alxjrvs@gmail.com>
2012-03-09 01:59:13 -0500

    created rollmany function to roll a number of dice of a given size. renamed roll to D (to make sense - D(sides) gives us D(20), etc.

--- roll.rb
+++ roll.rb
@@ -1,19 +1,24 @@
 $statlist = ['STR', 'DEX', 'CHA', 'INT', 'WIS', 'CON'] 
 
 # Roll a D(sides) size die. 
-def roll(sides)
+def D(sides)
 return rand(sides) + 1  
 end
+
+def rollmany(dice, sides) 
+    rolls = 0 
+    for i in (1..dice)
+        rolls = rolls + D(sides)
+    end
+return rolls
+end
+
 
 #This iterates through the Stat list and turns each stat into a key. It then rolls 3 d6's to get the starting value for that stat.
 def statroll() 
 stathash = {} 
     for stat in $statlist 
-        rolls = 0
-        for i in (1..3)
-            rolls =  rolls + roll(6) 
-        end
-        stathash[stat] = rolls  
+        stathash[stat] = rollmany(3, 6) 
     end 
 #prints the new hash 
 stathash.each {|key, value| puts "#{key} is #{value}" }
@@ -21,6 +26,7 @@ end
 
 
 #Class, what the hell are you doing here? No, It's okay. I'll move you later. 
+
 class Origin
     attr_accessor :name, :desc
 
