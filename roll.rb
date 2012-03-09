2661df2da341bfd6acb15f778a01847807d6c338
Alex Jarvis <alxjrvs@gmail.com>
2012-03-09 00:54:15 -0500

    Collapsed two functions into one.

--- roll.rb
+++ roll.rb
@@ -1,27 +1,20 @@
-def dice(sides)
+def roll(sides)
 return rand(sides) + 1  
 end
 
 
-def roller(num) 
-rolls = 0 
-    for i in (1..num)
-    rolls = rolls + dice(6)  
-    
-    end
-
-puts rolls
-return rolls
-end
 
 
 $statlist = ['STR', 'DEX', 'CHA', 'INT', 'WIS', 'CON'] 
 
 def statroll() 
-    stathash = {}
+stathash = {} 
     for stat in $statlist 
-        stathash[stat] = roller(3)
-        puts stat.to_s + " is " + stathash[stat].to_s
+        rolls = 0
+        for i in (1..3)
+            rolls =  rolls + roll(6) 
+        end
+        stathash[stat] = rolls  
     end 
 
 stathash.each {|key, value| puts "#{key} is #{value}" }
