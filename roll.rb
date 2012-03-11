00d37cb01cad58bf0673e098e25c6ddfec0f6239
Alex Jarvis <alxjrvs@gmail.com>
2012-03-11 06:49:39 -0400

    Huge markup. cleaned up the classes, now includes modifiers to skills.

--- roll.rb
+++ roll.rb
@@ -1,29 +1,58 @@
 $statlist = ['STR', 'DEX', 'CHA', 'INT', 'WIS', 'CON'] 
 
 # Roll a D(sides) size die. 
-def D(sides)
+def rollD(sides)
 return rand(sides) + 1  
 end
 
 
 # Rolls a number of dice of particular sizes.
-def rollmany(dice, sides) 
+def rollmanyD(dice, sides) 
     rolls = 0 
     for i in (1..dice)
-        rolls = rolls + D(sides)
+        rolls = rolls + rollD(sides)
     end
 return rolls
 end
 
+def modstat(r)
+modlist = []
+    if r == 3
+        modlist = [r, -4]
+    elsif (4..5).include? r
+        modlist = [r, -3]
+    elsif (6..7).include? r
+        modlist = [r, -2]
+    elsif (8..9).include? r
+        modlist = [r, -1]
+    elsif (10..11).include? r
+        modlist = [r, 0]
+    elsif (12..13).include? r
+        modlist = [r, 1]
+    elsif (14..15).include? r
+        modlist = [r, 2]
+    elsif (16..17).include? r
+        modlist = [r, 3]
+    elsif  r == 18
+        modlist = [r, 4]
+    else 
+        puts "nothing"
+    end
+    return modlist
+end
+
+    
 
 #This iterates through the Stat list and turns each stat into a key. It then rolls 3 d6's to get the starting value for that stat.
 def statroll() 
 stathash = {} 
     for stat in $statlist 
-        stathash[stat] = rollmany(3, 6) 
+        stathash[stat] = modstat(rollmanyD(3, 6)) 
     end 
+    return stathash
 #prints the new hash 
-stathash.each {|key, value| puts "#{key} is #{value}" }
+#stathash.each {|key, value| puts "#{key} is #{value}" }
+
 end
 
 
@@ -38,27 +67,17 @@ class Origin
     end
 end
 
-
 class PC 
-    class_accessor :name, :primo, :seco
+    attr_accessor :name, :primo, :seco, :stats
+
     def initialize(name, primo, seco) 
         @name = name
         @primo = primo
         @seco = seco
-    end
-end
-
-class Test
-    attr_accessor :stats
-
-    def initialize()
         @stats = statroll()
     end
 end
 
-def startup()
-    seismic = Origin.new("Seismic", "Rock Guy") 
-    android = Origin.new("Android", "Robot")
-    mark = PC.new("Mark", android, seismic)
 
-end
+#This line is going to be important later.
+#stat[0], stat[0][0], stat[0][1] 
